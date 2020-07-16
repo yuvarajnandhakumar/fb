f850adefe404a416501c4ce119961b0f353b1ef6
yuvarajnandhakumar <kajekarthi@gmail.com>
2020-07-16 20:44:18 +0530

    backgroung added in feature file

--- src/main/java/facebook/launch/facebook.java
+++ src/main/java/facebook/launch/facebook.java
@@ -10,24 +10,36 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class facebook {
-	
+
 	public static WebDriver driver;
-	@Given("User launches ChromeBrowser")
-	public void user_launches_ChromeBrowser() throws InterruptedException 
-	{
+
+
+	@Given("launching chrome browser")
+	public void launching_chrome_browser() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get("https://www.facebook.com/");
+
 		driver.manage().window().maximize();
 		System.out.println("browser maximized");
 		Thread.sleep(2000);	
-		
+
+	}
+
+
+
+	@Given("User launches fb")
+	public void user_launches_fb() throws InterruptedException 
+	{
+
+		driver.get("https://www.facebook.com/");
+
+
 	}
 
 
 	@And("Verify the Title")
 	public void verify_the_Title() {
-	    
+
 		String currentUrl = driver.getCurrentUrl();
 		System.out.println(currentUrl);
 
@@ -39,7 +51,7 @@ public class facebook {
 	public void user_enters_uname() throws InterruptedException {
 		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("yuvarajnandhak@gmail.com");
 		Thread.sleep(1000);
-		
+
 	}
 
 	@And("user enters pwd")
@@ -50,10 +62,32 @@ public class facebook {
 
 	@Then("user click submit button")
 	public void user_click_submit_button() {
-		
+
 		driver.findElement(By.xpath("//input[@value='Log In']")).click();
 		System.out.println("login button clicked");
+		driver.quit();
 	}
+
+	@Given("User launches Flipkart site")
+	public void User_launches_Flipkart_site() {
+		driver.get("http://flipkart.com");	
+		System.out.println("flipkart launched");
+		driver.quit();
+	}
+	
+	
+	@Then("user enter username as {string} and password {string}")
+	public void user_enter_username_as_and_password(String uname, String pwd) throws InterruptedException {
+	    
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys(uname);
+		Thread.sleep(1000);
+
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys(pwd);
+		Thread.sleep(1000);
+		
+	}
+
+
 
 
 }
--- src/main/resources/Feature/fb.feature
+++ src/main/resources/Feature/fb.feature
@@ -1,10 +1,18 @@
 #Author: yuvaraj
-
 Feature: My first feature file
-Scenario: Launch Fb
-Given User launches ChromeBrowser
-And Verify the Title
-When User enters uname 
-And user enters pwd
-Then user click submit button
 
+  Background: Chrome launch
+    Given launching chrome browser
+
+  Scenario: Launch Fb
+    Given User launches fb
+    Then user enter username as "ashok@gmail.com" and password "ashok78687"
+    #And Verify the Title
+    #When User enters uname
+    #And user enters pwd
+    #Then user click submit button
+    
+    
+
+    #Scenario: Launch Flipkart
+    #Given User launches Flipkart site
